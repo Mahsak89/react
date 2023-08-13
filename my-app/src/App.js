ba62f854ec2f944c139b5ad06da5b1e150271168
Mahsak89 <>
2023-08-13 23:55:07 +0000

    stateless function

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreeting />
+      <FunctionalGreetingWithProps greeting = "Nice to meet you!" name = "Sara" age = "25" />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+//function FinctionalGreeting(){
+//    return <h1>Hello from React!</h1>
+//}
+const FunctionalGreeting = () => <h1>Heloo from the outside</h1>;
+export default FunctionalGreeting;
