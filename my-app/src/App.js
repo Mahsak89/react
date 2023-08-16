eccca3551dbe7321dcd7012ba4d85516b807c951
Mahsak89 <>
2023-08-16 20:15:29 +0000

    change back to hello

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting = "Nice to meet you!" name = "Sara" age = "25" />
+      <StatefulGreeting  name="mahsa" />
     </div>
   );
 }
