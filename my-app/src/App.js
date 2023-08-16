f2fc0f2422885e14b09255bda48000f91fed2769
Mahsak89 <>
2023-08-16 22:48:48 +0000

    added he eventbinding.js in components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting = "Nice to meet you!" name = "Sara" age = "25" />
       <StatefulGreeting  name="mahsa" />
+      <hr/>
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -5,20 +5,38 @@ class StatefulGreeting extends React.Component{
         super(props);
         this.state ={
             introduction: "hello",
-            buttonText: "Exist",
+            buttonText: "Exit",
+            count : 0 ,
 
         };
     }
     handleClick(){
-        this.setState({
-            introduction: this.state.introduction === "hello" ? "goodbye" : "hello",
-            buttonText: "Enter",
-        }, ()=>{
-            console.log("new state",this.state.introduction);
-            console.log("new state",this.state.buttonText);
+        this.setState((prevState, prevProps) => {
+            console.log("Previous State:", prevState);
+            console.log("Previous Props:", prevProps);
+            return {
+              introduction:
+                prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+              buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+            };
+          });
+    }
+    increment() {
+        this.setState((prevState, prevProps) => {
+          console.log("Previous State:", prevState);
+          console.log("Previous Props:", prevProps);
+          return {
+            count: prevState.count + 1,
+          };
         });
-        console.log(this.state.introduction);
-        console.log(this.state.buttonText);
+      }
+  
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
 
     render() {
@@ -26,6 +44,10 @@ class StatefulGreeting extends React.Component{
             <div>
                 <h1> {this.state.introduction} {this.props.name}</h1>
                 <button onClick={()=>this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.incrementFive()}>
+                    Increment five
+                </button>
+                <p>You've clicked {this.state.count} times</p>
             </div>
         )
     }
